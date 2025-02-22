112466a727a4cae22af78ba7339a89b547a3c573
Youwen Wu <youwenw@gmail.com>
2025-02-22 14:53:55 -0800

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShellNoCC {
+            packages = with pkgs; [
+              nodejs
+              pnpm
+              typescript-language-server
+              svelte-language-server
+              tailwindcss-language-server
+            ];
+          };
+        }
+      );
+    };
+}
