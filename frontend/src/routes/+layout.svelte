96788067d6e51fe2c57e02d4030c2f9979f7861a
Youwen Wu <youwenw@gmail.com>
2025-02-24 16:31:55 -0800

    feat: add metadata

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -4,6 +4,11 @@
   let { children } = $props();
 </script>
 
+<svelte:head>
+	<title>Virion</title>
+	<meta name="description" content="An interactive epidemic modeler and visualizer in 3D for the H5N1 avian influenza." />
+</svelte:head>
+
 <Toaster />
 
 {@render children()}
